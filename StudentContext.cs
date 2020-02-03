57d9d0c7b6c6e6857a46cdd9c6bfb89b901f553e
Pelin Mutanguha <pelmut2000@gmail.com>
2020-02-03 15:31:19 +0200

    Update StudentContext.cs

--- StudentContext.cs
+++ StudentContext.cs
@@ -6,21 +6,11 @@ namespace CRUDStudentApp
 {
     public class StudentContext : DbContext
     {
-        
-        // private readonly string _connectionString;
-        //
-        // public StudentContext(string connectionString)
-        // {
-        //     _connectionString = connectionString;
-        // }
-        //
-   
         public DbSet<Student> Students { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            // optionsBuilder.UseSqlite("Data source=StudentDB.db");
-            // !!!: Use to complete path to your Database in your project
+          
             optionsBuilder.UseSqlite(@"Data Source=.\home\RiderProjects\CRUDStudentApp\CRUDStudentApp\StudentDB.db");
 
         }
